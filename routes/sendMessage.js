761928cacc64c1c741143c165c33f608e5cc1454
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-05-29 02:24:04 -0300

    sendMessage

---
+++ routes/sendMessage.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var cfg = require('../config.json');
+var axios = require('axios');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var data = JSON.stringify({
+    "messaging_product": "whatsapp",
+    "to": cfg.recipientWAID,
+    "type": "template",
+    "template": {
+      "name": "hello_world",
+      "language": {
+        "code": "en_US"
+      }
+    }
+  });
+  
+  var config = {
+    method: 'post',
+    url: `https://graph.facebook.com/${cfg.version}/${cfg.phoneNumberID}/messages`,
+    headers: { 
+      'Authorization': `Bearer ${cfg.userAccessToken}`, 
+      'Content-Type': 'application/json'
+    },
+    data : data
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+});
+
+module.exports = router;
+
+
