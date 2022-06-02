09ba98840f74aa85e4c0c58304395c62203637c3
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-06-02 01:11:15 -0300

    DB_DATABASE

--- routes/webHook.js
+++ routes/webHook.js
@@ -55,7 +55,8 @@ router.post('/', function(req, res, next) {
   var con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
   });
   
   con.connect(function(err) {
