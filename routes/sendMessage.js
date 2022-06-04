2cd95d55f606acac240838730d3ea1d8102d8155
marcelo oliveira <marcelo.r.oliveira@ebradi.com.br>
2022-06-04 17:00:28 -0300

    .

--- routes/sendMessage.js
+++ routes/sendMessage.js
@@ -21,40 +21,30 @@ router.get('/', function(req, res, next) {
         "code": "en_US"
       },
       "components": [{
-                        "type" : "header",
-                        "parameters": [
-                            {
-                                "type": "text",
-                                "text": "THIS IS A SILLY TITLE"
-                            }
-                        ]
-                      },
-                      {
                           "type" : "body",
-                          "parameters": [
-                        {
-                          "type": "text",
-                          "text": "Top Gun 2"
-                        },
-                        {
-                          "type": "date_time",
-                            "date_time" : {
-                                "fallback_value": "May 1st, 2019 8:45pm",
-                                "day_of_month": 20,
-                                "year": 2019,
-                                "month": 9,
-                                "hour": 20,
-                                "minute": 45
-                            }
-                        },
-                        {
-                          "type": "text",
-                          "text": "Houston Grand Cinema"
-                        },
-                        {
-                          "type": "text",
-                          "text": "3"
-                        }]
+                          "parameters": [{
+                                          "type": "text",
+                                          "text": "Top Gun 2"
+                                        },
+                                        {
+                                          "type": "date_time",
+                                          "date_time" : {
+                                              "fallback_value": "May 1st, 2019 8:45pm",
+                                              "day_of_month": 20,
+                                              "year": 2019,
+                                              "month": 9,
+                                              "hour": 20,
+                                              "minute": 45
+                                          }
+                                        },
+                                        {
+                                          "type": "text",
+                                          "text": "Houston Grand Cinema"
+                                        },
+                                        {
+                                          "type": "text",
+                                          "text": "3"
+                                        }]
                       }]
     }
   });
